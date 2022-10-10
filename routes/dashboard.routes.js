9886872a802a50830cab21997ba40b982c09070a
Bidz42 <abid97r@gmail.com>
2022-10-10 10:35:16 +0100

    edit route

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -11,5 +11,15 @@ ContactForm.find()
     .catch(err => console.log(err))
 })
 
+router.post("/edit" , (req,res) => {
+    const {id, targetValueEdit} = req.body
+    console.log('body',req.body)
+ContactForm.findByIdAndUpdate({_id : id}, {status : targetValueEdit}, {new:true})
+    .then(response => {
+        console.log(response)
+        res.status(200).json(response)})
+    .catch(err => console.log(err))
+})
+
 
 module.exports = router;
